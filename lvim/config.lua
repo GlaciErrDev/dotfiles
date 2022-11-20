0143e1a7de6531f6b05a7243fae6772d2e2db3be
Mykhailo Durnosvystov <GlaciErrDev@users.noreply.github.com>
2022-11-19 19:16:42 -0700

    Update lvim config

--- lvim/config.lua
+++ lvim/config.lua
@@ -10,33 +10,84 @@ an executable
 
 -- general
 lvim.log.level = "warn"
-lvim.format_on_save = true
+lvim.format_on_save = false
 lvim.colorscheme = "newpaper"
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+
+
+require("nvim-tree").setup({
+  sync_root_with_cwd = true,
+  respect_buf_cwd = true,
+  update_focused_file = {
+    enable = true,
+    update_root = true
+  },
+})
+
+lvim.builtin.which_key.mappings["l"]["f"] = {
+  function()
+    require("lvim.lsp.utils").format { timeout_ms = 5000 }
+  end,
+  "Format",
+}
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
 lvim.builtin.bufferline.options.always_show_bufferline = true
+lvim.builtin.breadcrumbs.active = false
+lvim.builtin.nvimtree.setup.view.width = 60
 
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
 -- add your own keymapping
 -- NORMAL MODE
 lvim.keys.normal_mode["<C-s>"] = ":wa<cr>"
-lvim.keys.normal_mode["<leader>."] = ":BufferLineCycleNext<CR>"
-lvim.keys.normal_mode["<leader>m"] = ":BufferLineCyclePrev<CR>"
+lvim.keys.normal_mode["<leader>k"] = ":BufferLineCycleNext<CR>"
+lvim.keys.normal_mode["<leader>j"] = ":BufferLineCyclePrev<CR>"
 
 lvim.keys.normal_mode["<leader>sl"] = ":NewpaperLight<CR>"
 lvim.keys.normal_mode["<leader>sd"] = ":NewpaperDark<CR>"
 
 lvim.keys.normal_mode["<C-p>"] = ":MarkdownPreviewToggle<CR>"
 
+lvim.keys.normal_mode["<leader><space>"] = "<cmd>lua require('telescope.builtin').resume()<CR>"
+
 -- unmap a default keymapping
 -- vim.keymap.del("n", "<C-Up>")
 -- override a default keymapping
-lvim.keys.normal_mode["<space>q"] = ":q<cr>" -- or vim.keymap.set("n", "<C-q>", ":q<cr>" )
+lvim.keys.normal_mode["<space>q"] = ":qa!<cr>" -- or vim.keymap.set("n", "<C-q>", ":q<cr>" )
+
+-- INSERT MODE
+lvim.keys.insert_mode["kj"] = "<Esc>"
+
+
 
 -- VISUAL MODE
-lvim.keys.visual_mode["<leader>st"] = "<cmd>Telescope grep_string<CR>"
 
+local tb = require('telescope.builtin')
+
+lvim.keys.visual_mode["<leader>st"] = function()
+  local text = vim.getVisualSelection()
+  tb.current_buffer_fuzzy_find({ default_text = text })
+end
+lvim.keys.visual_mode["<leader>sT"] = function()
+  local text = vim.getVisualSelection()
+  tb.live_grep({ default_text = text })
+end
+
+
+function vim.getVisualSelection()
+  vim.cmd('noau normal! "vy"')
+  local text = vim.fn.getreg('v')
+  vim.fn.setreg('v', {})
+
+  text = string.gsub(text, "\n", "")
+  if #text > 0 then
+    return text
+  else
+    return ''
+  end
+end
 
 -- Additional Plugins
 -- lvim.plugins = {
@@ -74,6 +125,8 @@ lvim.plugins = {
       }
     end
   },
+  { "tpope/vim-surround", keys = { "c", "d", "y" } },
+  { "kdheepak/lazygit.nvim" }
 }
 
 
@@ -97,21 +150,20 @@ lvim.builtin.telescope.defaults.mappings = {
 
 -- Use which-key to add extra bindings with the leader-key prefix
 -- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
-lvim.builtin.which_key.mappings["t"] = {
-  name = "+Trouble",
-  r = { "<cmd>Trouble lsp_references<cr>", "References" },
-  f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
-  d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
-  q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
-  l = { "<cmd>Trouble loclist<cr>", "LocationList" },
-  w = { "<cmd>Trouble workspace_diagnostics<cr>", "Workspace Diagnostics" },
-}
+-- lvim.builtin.which_key.mappings["t"] = {
+--   name = "+Trouble",
+--   r = { "<cmd>Trouble lsp_references<cr>", "References" },
+--   f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
+--   d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
+--   q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
+--   l = { "<cmd>Trouble loclist<cr>", "LocationList" },
+--   w = { "<cmd>Trouble workspace_diagnostics<cr>", "Workspace Diagnostics" },
+-- }
 
 -- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.alpha.active = false
 lvim.builtin.alpha.mode = "dashboard"
-lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = false
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = true
@@ -119,7 +171,6 @@ lvim.builtin.nvimtree.setup.renderer.icons.show.git = true
 -- no need to set style = "lvim"
 lvim.builtin.lualine.style = "lvim"
 local components = require("lvim.core.lualine.components")
-
 lvim.builtin.lualine.sections.lualine_a = { "mode" }
 lvim.builtin.lualine.sections.lualine_y = {
   components.location
@@ -188,14 +239,17 @@ lvim.lsp.installer.setup.ensure_installed = {
 -- set a formatter, this will override the language server formatting capabilities (if it exists)
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
-  { command = "black", filetypes = { "python" } },
-  { command = "isort", filetypes = { "python" } },
+  { command = "black",
+    extra_args = { "--line-length", "120" },
+    filetypes = { "python" },
+  },
+  -- { command = "isort", filetypes = { "python" } },
   {
     -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
     command = "prettier",
     ---@usage arguments to pass to the formatter
     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
-    extra_args = { "--print-with", "100" },
+    extra_args = { "--print-with", "120" },
     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
     filetypes = { "typescript", "typescriptreact" },
   },
@@ -204,10 +258,25 @@ formatters.setup {
 -- set additional linters
 local linters = require "lvim.lsp.null-ls.linters"
 linters.setup {
-  { command = "flake8", filetypes = { "python" } },
-  { command = "mypy", filetypes = { "python" } },
-  { command = "pylint", filetypes = { "python" } },
-  { command = "markdownlint", filetypes = { "markdown" } },
+  {
+    command = "flake8",
+    filetypes = { "python" },
+    extra_args = { "--config", "~/.config/flake8" }
+  },
+  -- {
+  --   command = "mypy",
+  --   filetypes = { "python" },
+  -- },
+  {
+    command = "pylint",
+    filetypes = { "python" },
+    extra_args = { "--max-line-length", "120" }
+  },
+  {
+    command = "markdownlint",
+    filetypes = { "markdown" },
+    extra_args = { "--disable", "MD013" },
+  },
   {
     -- each linter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
     command = "shellcheck",
@@ -219,7 +288,7 @@ linters.setup {
     command = "codespell",
     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
     filetypes = { "javascript", "python" },
-  },
+  }
 }
 
 -- -- set a code actions
@@ -232,15 +301,12 @@ linters.setup {
 
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
--- vim.api.nvim_create_autocmd("BufEnter", {
---   pattern = { "*.json", "*.jsonc" },
---   -- enable wrap mode for json files only
---   command = "setlocal wrap",
--- })
--- vim.api.nvim_create_autocmd("FileType", {
---   pattern = "zsh",
---   callback = function()
---     -- let treesitter use bash highlight for zsh files as well
---     require("nvim-treesitter.highlight").attach(0, "bash")
---   end,
--- })
+-- local customGroup = vim.api.nvim_create_augroup("CustomGroup", { clear = true })
+-- vim.api.nvim_create_autocmd(
+--   {"BufEnter", "BufNew"},
+--   {
+--     pattern = { "*" },
+--     group = customGroup,
+--     command = "normal zR",
+--   }
+-- )
