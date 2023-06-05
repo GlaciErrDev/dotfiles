dbd8a4e8ef9726dfb9ddb1505260dddd823d0cde
Mike Durnosvistov <GlaciErrDev@users.noreply.github.com>
2023-06-04 20:30:44 -0600

    Remove LazyVim; Update keymap

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -1,4 +1,4 @@
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
-vim.keymap.set('n', '<space>q', ':wq!<CR>')
+vim.keymap.set('n', '<space>q', ':qa!<CR>')
 vim.keymap.set('n', '<C-s>', ':wa<CR>')
 vim.keymap.set('i', 'kj', '<ESC>')
