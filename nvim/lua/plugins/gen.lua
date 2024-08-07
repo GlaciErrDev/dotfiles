34fa6bf52c2bb67207dc94f206243dc74e02f32e
Mike Durnosvystov <GlaciErrDev@users.noreply.github.com>
2024-08-07 11:56:07 +0200

    Update dotfiles
    * Add to yt-dlp-h alias '--write-description' option
    * Update nvim keymaps
    * Change default model for gen.nvim

--- nvim/lua/plugins/gen.lua
+++ nvim/lua/plugins/gen.lua
@@ -2,7 +2,7 @@ return {
   {
     "David-Kunz/gen.nvim",
     opts = {
-      model = "llama3.1:70b", -- The default model to use.
+      model = "llama3.1", -- The default model to use.
     },
   },
 }
